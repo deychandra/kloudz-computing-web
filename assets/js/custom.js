feaead626233d1f26201e601ace5a90953aa2056
chandra <deychandrasekhar190@gmail.com>
2025-01-15 02:03:44 +0530

    djfjdjf

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,3 +12,19 @@ document.addEventListener('mousemove', (event) => {
 
     glowImage.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
 });
+
+const weserveImageGlobe = document.querySelector('.we-serve-move');
+
+document.addEventListener('mousemove', (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    const offsetX = (mouseX / windowWidth - 0.5) * 40;
+    const offsetY = (mouseY / windowHeight - 0.5) * 40;
+
+    weserveImageGlobe.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
+});
+
