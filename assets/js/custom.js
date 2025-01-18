ed6f153755aff773b369df0f43518b4a1059cef3
chandra <deychandrasekhar190@gmail.com>
2025-01-18 16:45:10 +0530

    djfjdjf

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,50 +1,162 @@
 const glowImage = document.querySelector('.glow-move');
+const whyChooseUsSection = document.querySelector('.why-choose-us');
 
-document.addEventListener('mousemove', (event) => {
+let offsetD = 0;
+let offsetC = 0;
+
+whyChooseUsSection.addEventListener('mousemove', (event) => {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
 
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
 
-    const offsetX = (mouseX / windowWidth - 0.5) * 50;
-    const offsetY = (mouseY / windowHeight - 0.5) * 50;
+
+    offsetX = (mouseX / windowWidth - 0.5) * 50;
+    offsetY = (mouseY / windowHeight - 0.5) * 50;
+
 
     glowImage.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
 });
 
-const weserveImageGlobe = document.querySelector('.we-serve-move');
 
-document.addEventListener('mousemove', (event) => {
+whyChooseUsSection.addEventListener('mouseenter', () => {
+
+    glowImage.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
+});
+
+
+whyChooseUsSection.addEventListener('mouseleave', () => {
+
+    glowImage.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
+});
+
+
+
+
+
+
+// animation
+const weServeImageGlobe = document.querySelector('.we-serve-move');
+const weServeSection = document.querySelector('.We-Serve');
+
+let offsetXh = 0;
+let offsetYw = 0;
+
+
+weServeSection.addEventListener('mousemove', (event) => {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
 
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
 
-    const offsetX = (mouseX / windowWidth - 0.5) * 40;
-    const offsetY = (mouseY / windowHeight - 0.5) * 40;
 
-    weserveImageGlobe.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
+    offsetX = (mouseX / windowWidth - 0.5) * 40;
+    offsetY = (mouseY / windowHeight - 0.5) * 40;
+
+
+    weServeImageGlobe.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
+});
+
+
+weServeSection.addEventListener('mouseenter', () => {
+
+    weServeImageGlobe.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
+});
+
+
+weServeSection.addEventListener('mouseleave', () => {
+
+    weServeImageGlobe.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
 });
 
 
 
 
-const CaseStudiesGlobe = document.querySelector('.case-studies-glow');
 
-document.addEventListener('mousemove', (event) => {
+
+
+
+
+// animation
+const caseStudiesGlobe = document.querySelector('.case-studies-glow');
+const caseStudiesSection = document.querySelector('.case-studies');
+
+let isMouseInsidee = false;
+let offsetXx = 0;
+let offsetYy = 0;
+
+
+caseStudiesSection.addEventListener('mousemove', (event) => {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
 
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
 
-    const offsetX = (mouseX / windowWidth - 0.5) * 40;
-    const offsetY = (mouseY / windowHeight - 0.5) * 40;
 
-    CaseStudiesGlobe.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
+    offsetX = (mouseX / windowWidth - 0.5) * 50;
+    offsetY = (mouseY / windowHeight - 0.5) * 50;
+
+
+    caseStudiesGlobe.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
 });
+
+
+caseStudiesSection.addEventListener('mouseenter', () => {
+    isMouseInside = true;
+
+    caseStudiesGlobe.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
+});
+
+
+caseStudiesSection.addEventListener('mouseleave', () => {
+    isMouseInside = false;
+
+    caseStudiesGlobe.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
+});
+
+
+
+// animation
+const getInTouch = document.querySelector('.get-in-touch-glow');
+const getInTouchSection = document.querySelector('.get-in-touch');
+
+let isMouseInside = false;
+let offsetX = 0;
+let offsetY = 0;
+
+
+getInTouchSection.addEventListener('mousemove', (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+
+    offsetX = (mouseX / windowWidth - 0.5) * 30;
+    offsetY = (mouseY / windowHeight - 0.5) * 30;
+
+
+    getInTouch.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
+});
+
+
+getInTouchSection.addEventListener('mouseenter', () => {
+    isMouseInside = true;
+
+    getInTouch.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
+});
+
+
+getInTouchSection.addEventListener('mouseleave', () => {
+    isMouseInside = false;
+
+    getInTouch.style.transform = `translate(-50%, -50%) translate(${offsetX}%, ${offsetY}%)`;
+});
+
 
 
 
@@ -84,12 +196,12 @@ tabScroll.addEventListener('mousemove', (e) => {
 var swiper = new Swiper(".mySwiper", {
     cssMode: true,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
     pagination: {
-      el: ".swiper-pagination",
+        el: ".swiper-pagination",
     },
     mousewheel: true,
     keyboard: true,
-  });
+});
